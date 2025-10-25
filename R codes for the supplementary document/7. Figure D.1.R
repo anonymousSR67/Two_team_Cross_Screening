9851f0239563ae68f9df769beba4d358ed496ed0
anonymousSR67 <samratr@iima.ac.in>
2025-10-25 03:06:01 -0400

    Update 7. Figure D.1.R

--- R codes for the supplementary document/7. Figure D.1.R
+++ R codes for the supplementary document/7. Figure D.1.R
@@ -5,13 +5,12 @@ library(xtable)
 library(lsr)
 library(ggplot2)
 library(sensitivitymw)
+library(here)
 #=================Data preparation============
-setwd("C:/Study_2023/Cross_Screen/Data_storage")
 
-cath_match <- read.csv("Catholics_matched_pairs.csv")
+data <- read_dta(here("Data", "WLS_master_data.dta"))
+cath_match <- read.csv(here("Data", "Catholics_matched_pairs.csv"))
 cath_match <- cath_match[,-c(1)]
-
-data <- read_dta(file = "WLS_master_data.dta")
 data_f <- data.frame(data)
 data_f <- data_f[which(data_f$rtype=="g"),] #filter: graduate
 data_f <- data_f[which(data_f$z_sexrsp==2),] #filter: female
@@ -39,3 +38,4 @@ boxplot(income_dat[,2], income_dat[,5], main="total income")
 
 #======Wages and salariesboxplot======  
 boxplot(income_dat[,1], income_dat[,6], main="Wages and salaries")
+
